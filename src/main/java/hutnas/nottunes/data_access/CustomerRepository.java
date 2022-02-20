07d79060b4bd85f972754e19d7fe728b5b1a27e5
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-02-20 09:56:32 +0200

    fix(database connection): added ConnectionHelper, reorganized connection to getAllCustomers, tested with Postman and returns customers

--- src/main/java/hutnas/nottunes/data_access/CustomerRepository.java
+++ src/main/java/hutnas/nottunes/data_access/CustomerRepository.java
@@ -3,10 +3,59 @@ package hutnas.nottunes.data_access;
 import hutnas.nottunes.models.Customer;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.ArrayList;
 
-@Repository
-public interface CustomerRepository {
-    public ArrayList<Customer> getAllCustomers();
+
+public class CustomerRepository {
+    // Setting up the connection object
+    private String URL = ConnectionHelper.CONNECTION_URL;
+    private Connection conn = null;
+
+
+    public ArrayList<Customer> getAllCustomers() {
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        try {
+            // Open Connection
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Connection to Chinook SQLite established.");
+
+            // Prepare Statement
+            PreparedStatement preparedStatement =
+                    conn.prepareStatement("SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email from Customer");
+            // Execute Statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Process Results
+            while (resultSet.next()) {
+                customers.add(
+                        new Customer(
+                                resultSet.getString("CustomerID"),
+                                resultSet.getString("FirstName"),
+                                resultSet.getString("LastName"),
+                                resultSet.getString("Country"),
+                                resultSet.getString("PostalCode"),
+                                resultSet.getString("Phone"),
+                                resultSet.getString("Email")
+                        )
+                );
+            }
+
+
+        } catch (Exception ex) {
+            System.out.println("Something went wrong...");
+            System.out.println(ex.toString());
+        } finally {
+            try {
+                // Close Connection
+                conn.close();
+            } catch (Exception ex) {
+                System.out.println("Something went wrong while closing the connection.");
+                System.out.println(ex.toString());
+            }
+        }
+        return customers;
+    }
 
 }
--- src/main/java/hutnas/nottunes/models/Customer.java
+++ src/main/java/hutnas/nottunes/models/Customer.java
@@ -19,6 +19,62 @@ public class Customer {
         this.email = email;
     }
 
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
