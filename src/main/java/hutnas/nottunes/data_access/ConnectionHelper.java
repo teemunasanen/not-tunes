8725f200bd97e1e697393dda4e1e4d881fde4890
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-02-21 17:23:09 +0200

    fix: connection url

--- src/main/java/hutnas/nottunes/data_access/ConnectionHelper.java
+++ src/main/java/hutnas/nottunes/data_access/ConnectionHelper.java
@@ -4,6 +4,6 @@ import java.sql.Connection;
 
 public class ConnectionHelper {
     // Global access for Connection Url
-    public static final String CONNECTION_URL = "jdbc:sqlite::resource:Chinook_Sqlite.sqlite";
+    public static final String CONNECTION_URL = "jdbc:sqlite:src/main/resources/Chinook_Sqlite.sqlite";
 
 }
